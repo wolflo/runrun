af3c648726966925028b7e2ae3958381452c49d0
wolflo <33909953+wolflo@users.noreply.github.com>
2021-12-26 17:18:52 -0700

    Type level outline

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-#![feature(generic_associated_types)]
-#![feature(unboxed_closures)]
-#![feature(fn_traits)]
+// #![feature(generic_associated_types)]
+// #![feature(unboxed_closures)]
+// #![feature(fn_traits)]
+
+// far from being stable it sounds like
+#![feature(generic_const_exprs)]
 
 #![allow(unused_variables)]
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-pub mod core;
-pub mod hooks;
+pub mod ty;
---
+++ src/ty.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use futures::future::Future;
+use std::marker::PhantomData;
+
+
+pub struct TNil;
+pub struct TCons<H, T>(PhantomData<(H, T)>);
+
+// have a list of types
+// need a fn (trait) that takes a list of types and a (list of?) function on those types and accumulated the results
+// Runner should be built from initial state that user designates with #[run_state(init)]
+//  - should take only a context and a list of tests -- see proptest lib
+pub trait Runner {
+    type Ctx;
+    fn run(ctx: &Self::Ctx, acts: &'static [fn(Self::Ctx)]) -> Result<()>;
+    // build from previous runner?
+}
+
+// Need action on binder to:
+// Create state from previous state
+// Build runner
+// Get and run tests
+// Get and bind children
+
+pub trait ActSet {
+    type Ctx;
+    type TPtrs;
+    fn tests() -> &'static [fn(Self::Ctx)];
+}
+
+struct NullCtx;
+type CtxInit = NullCtx;
+
+// take N elements of a TList
+pub trait Take<const N: usize> { type Ret; }
+impl Take<0> for TNil { type Ret = TNil; }
+impl<H, T, const N: usize> Take<N> for TCons<H, T> where T: Take<{N-1}> { type Ret = TCons<H, <T as Take<{N-1}>>::Ret>; }
+
+pub fn main() {
+    // CtxInit::bind();
+}
