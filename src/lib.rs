d6f7106e3bb4b4491b6335748ec087f694c78387
wolflo <33909953+wolflo@users.noreply.github.com>
2022-01-23 08:47:46 -0700

    Move new implementation to original filenames

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-#![feature(generic_associated_types)]
 #![allow(dead_code)]
 
-pub mod core_stream;
-pub mod eth_stream;
-pub mod hooks_stream;
-pub mod macros;
 pub mod types;
+pub mod core;
+pub mod macros;
+pub mod hook;
+pub mod eth;
 
 pub use runrun_derive::*;
